ae2edca6375d3a86fb3b3ee608b5d2d3f243885c
Piyush <piyush8311@gmail.com>
2017-11-20 00:49:40 +0530

    Add Abstract syntax tree structure

---
+++ ast/ast.go
@@ -0,0 +1,52 @@
+package ast
+
+import "monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Root Node of each AST
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// Let Statement structure implements Node and Statement interfaces
+type LetStatement struct {
+	Token token.Token
+	Name *Identifier
+	Value Expression
+}
+
+/* Identifier implemets Node and Expression interfaces. It implements
+   Expression interface because indentifiers are used as expressions on
+   right side of Let Statements
+*/
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
